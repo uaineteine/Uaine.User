0e578910db03cfa9231643d94833dd08b3dd1f12
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-08-10 11:09:39 +1000

    --1.1-- Credentials for User is now protected User accounts can be hashed via the HashUser class

---
+++ HashUser.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace Uaine.Users
+{
+    public class HashUser : User
+    {
+        // public int TruncSize { get; set; }
+
+        public HashUser(User user) : 
+            base(Sha(user.Name), "", user.UserGroupID, user.Rank, user.ID)
+        {
+            ResetPassword(Sha(Credentials.Password));
+        }
+
+        public static string Sha(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = sha.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes); 
+            }
+        }
+    }
+}
+
